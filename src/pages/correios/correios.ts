e5bf40265adadae5600fa57aaa7423586c29f161
Andre Barros <andrebatera84@hotmail.com>
2019-03-15 21:39:34 -0300

    app01aula

--- src/pages/correios/correios.ts
+++ src/pages/correios/correios.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the CorreiosPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-correios',
@@ -15,11 +8,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CorreiosPage {
 
+  txtCidade: String;
+  txtEstado: String;
+  txtLogradouro: String;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CorreiosPage');
+  }
+
+  exibeInputs() {
+    console.log(`Cidade: ${this.txtCidade}\n Estado: ${this.txtEstado}\n Logradouro: ${this.txtLogradouro}`);
   }
 
 }
